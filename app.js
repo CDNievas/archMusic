56fbaea1f7e7a38607545a6b482b6b3bccb9a344
CDNievas <cdnievas@hotmail.com>
2020-04-04 14:23:29 -0300

    Added quantity of server joined on status

--- app.js
+++ app.js
@@ -8,15 +8,26 @@ const {
 } = require("./config.json");
 
 // Listener: Init Bot
-bot.on("ready", () => {
+bot.on("ready", async () => {
     console.log("Arch Music by CDNievas");
     console.log("Node Version: " + process.version);
     console.log("Discord.js Version: " + Discord.version);
+
+    await bot.user.setActivity((bot.guilds.cache.size).toString() + " servers !help", {type: "PLAYING"});
+
 });
+
+bot.on("guildCreate", async () => {
+
+    await bot.user.setActivity((bot.guilds.cache.size).toString() + " servers !help", {type: "PLAYING"});
+
+})
 
 // Listener: Receive Messages
 bot.on("message", async message => {
     handlerMsg.analizar(message);
 });
+
+
 
 bot.login(token);
